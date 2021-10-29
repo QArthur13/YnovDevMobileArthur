5dbcba3dff0c63045688b5dc6b42c24331334cef
Quaranta Arthur <arthur.quaranta@ynov.com>
2021-10-29 15:00:22 +0200

    Ajout du Hors-ligne

--- www/sw.js
+++ www/sw.js
@@ -1,47 +1,88 @@
-const OFFLINE_VERSION = 1;
+const ORIGIN_URL = `${location.protocol}//${location.host}`;
 const CACHE_NAME = "offline";
 const OFFLINE_URL = "offline.html";
+const CACHED_FILES = [
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
-    })()
-  );
-  self.skipWaiting();
-});
+  OFFLINE_URL,
+  `${ORIGIN_URL}/css/index.css`,
+  `${ORIGIN_URL}/js/index.js`,
+  `${ORIGIN_URL}/img/logo.png`,
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
-  );
-  self.clients.claim();
-});
+]
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResponse = await event.preloadResponse;
+const sendOfflinePage = (resolve) => {
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.match(OFFLINE_URL).then((cachedResponse) => {
+        resolve(cachedResponse);
+      });
+    });
+  };
+  
+  const respondWithFetchPromiseNavigate = (event) =>
+    new Promise((resolve) => {
+      event.preloadResponse
+        .then((preloadResponse) => {
           if (preloadResponse) {
-            return preloadResponse;
+            resolve(preloadResponse);
           }
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch (error) {
-          console.log("Fetch failed; returning offline page instead.", error);
+  
+          fetch(event.request)
+            .then((networkResponse) => {
+              resolve(networkResponse);
+            })
+            .catch(() => sendOfflinePage(resolve));
+        })
+        .catch(() => sendOfflinePage(resolve));
+    });
+  
+  const fetchSW = (event) => {
 
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+    if (event.request.mode === "navigate") {
+      event.respondWith(respondWithFetchPromiseNavigate(event));
+    } else if (CACHED_FILES.includes(event.request.url)) {
+      event.respondWith(caches.match(event.request));
+    }
+  };
+
+  const deleteOldCaches = () =>
+    new Promise((resolve) => {
+      caches.keys().then((keys) => {
+        Promise.all(
+          keys.map((key) => {
+            if (key !== CACHE_NAME) {
+              caches.delete(key);
+            }
+          })
+        ).finally(resolve);
+      });
+    });
+  
+  const waitUntilActivatePromise = () =>
+    new Promise((resolve) => {
+      deleteOldCaches().then(() => {
+        if ("navigationPreload" in self.registration) {
+          self.registration.navigationPreload.enable().finally(resolve);
         }
-      })()
-    );
-  }
-});
+      });
+    });
+  
+  const activate = (event) => {
+    event.waitUntil(waitUntilActivatePromise());
+    self.clients.claim();
+  };
+  
+  const waitUntilInstallationPromise = () =>
+    new Promise((resolve) => {
+      caches.open(CACHE_NAME).then((cache) => {
+        cache.addAll(CACHED_FILES).then(resolve);
+      });
+    });
+  
+  const installSW = (event) => {
+    event.waitUntil(waitUntilInstallationPromise());
+    self.skipWaiting();
+  };
+
+  self.addEventListener("install", installSW);
+  self.addEventListener("activate", activate);
+  self.addEventListener("fetch", fetchSW);
