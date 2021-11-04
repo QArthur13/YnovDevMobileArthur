59d5d76b86661dda672eb5c50a59034ac9552632
Quaranta Arthur <arthur.quaranta@ynov.com>
2021-11-04 18:50:24 +0100

    Finis

--- www/js/creation.js
+++ www/js/creation.js
@@ -6,16 +6,27 @@ function store() {
     let data = {
 
         'title': $("#title").val(),
-        'imageName': $("#imageName").val(),
-        'imageURL': $("#imageURL").val()
+        'imageName1': $("#imageName1").val(),
+        'imageURL1': $("#imageURL1").val(),
+        /* 'imageName2': $("#imageName2").val(),
+        'imageURL2': $("#imageURL2").val(),
+        'imageName3': $("#imageName3").val(),
+        'imageURL3': $("#imageURL3").val() */
 
     };
 
-    let jsonData = JSON.stringify(data);
-    console.log(jsonData);
+    /* let jsonData = JSON.stringify(data);
+    console.log(jsonData); */
 
-    localStorage.setItem('list', jsonData);
+    //localStorage.setItem('list', jsonData);
+    localStorage.setItem('list', JSON.stringify(data));
 
-    window.location("../index.html");
+    let test = JSON.parse(localStorage.getItem('list'));
+
+    console.log(test.imageName1 + ' ' + test.imageURL1);
+
+    document.getElementById("title").innerHTML = test.title;
+    document.getElementById("imageName").innerHTML = test.imageName1;
+    document.getElementById("imageURL").innerHTML = test.imageURL1;
 
 }
